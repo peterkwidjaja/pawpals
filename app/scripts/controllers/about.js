b4f3d56788918a07d77a65f83cd42b3db97b9864
Peter Kristianto Widjaja <peterkwidjaja@gmail.com>
2015-07-26 17:38:33 +0800

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pawpalsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the pawpalsApp
+ */
+angular.module('pawpalsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pawpalsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pawpalsApp
+ */
+angular.module('pawpalsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
